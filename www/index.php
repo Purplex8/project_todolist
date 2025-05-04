bdb32c91747fcbc782c3bddf872e0f8a0001f4af
Alexey Antonyk <aleksey-77.92@mail.ru>
2025-05-04 14:51:01 +0400

    layout breakdown

--- www/index.php
+++ www/index.php
@@ -1,9 +1,6 @@
 <?php
 //конфигурация
     require_once('./config.php');
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -14,44 +11,22 @@
 <body class="todo-app p-5">
 
 	<!-- Header -->
-	<header class="d-flex align-items-end mb-3">
-		<h1 class="m-0 me-2">Список дел</h1>
-		<span class="text-secondary">3 осталось, 0 сделано</span>
-	</header>
+    <?php
+        include(ROOT . 'templates/page_parts/header.tpl');
+    ?>
 
 	<!-- List -->
 	<ul class="list-group mb-3">
-		<li class="list-group-item d-flex justify-content-center">
-			<span class="todo-item-text">Список дел пуст</span>
-		</li>
-		<li class="list-group-item d-flex justify-content-between">
-			<span class="todo-item-text">Выпить кофе</span>
-			<div class="btn-group">
-				<button role="button" class="btn btn-outline-dark btn-sm">Важное</button>
-				<button role="button" class="btn btn-outline-danger btn-sm">Удалить</button>
-			</div>
-		</li>
-		<li class="list-group-item d-flex justify-content-between">
-			<span class="todo-item-text d-flex justify-content-between">Сделать React приложение</span>
-			<div class="btn-group">
-				<button role="button" class="btn btn-outline-dark btn-sm">Важное</button>
-				<button role="button" class="btn btn-outline-danger btn-sm">Удалить</button>
-			</div>
-		</li>
-		<li class="list-group-item d-flex justify-content-between">
-			<span class="todo-item-text">Позавтракать</span>
-			<div class="btn-group">
-				<button role="button" class="btn btn-outline-dark btn-sm">Важное</button>
-				<button role="button" class="btn btn-outline-danger btn-sm">Удалить</button>
-			</div>
-		</li>
+        <?php
+            include(ROOT . 'templates/empty.tpl');
+            include(ROOT . 'templates/task.tpl');
+        ?>
 	</ul>
 
-	<!-- Footer -->
-	<footer class="d-flex">
-		<input type="text" placeholder="Что необходимо сделать" class="form-control me-2" />
-		<button type="button" class="btn btn-primary">Добавить</button>
-	</footer>
+	<!-- form -->
+    <?php
+        include(ROOT . 'templates/page_parts/form.tpl');
+    ?>
 
 </body>
 </html>
---
+++ www/templates/task.tpl
@@ -0,0 +1,7 @@
+<li class="list-group-item d-flex justify-content-between">
+    <span class="todo-item-text">Выпить кофе</span>
+    <div class="btn-group">
+        <button role="button" class="btn btn-outline-dark btn-sm">Важное</button>
+        <button role="button" class="btn btn-outline-danger btn-sm">Удалить</button>
+    </div>
+</li>
